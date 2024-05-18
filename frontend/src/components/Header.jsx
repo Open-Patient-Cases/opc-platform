665381e19f63abf1c9392ecbb5dafba6f2bbc2b5
martinappberg <mkjberg@gmail.com>
2024-05-18 14:10:48 -0700

    Some fixes

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,8 @@
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
 import { Helmet } from "react-helmet";
 import PropTypes from "prop-types";
 import { useSubdomain } from "../context/SubdomainContext";
@@ -10,20 +11,31 @@ const Header = () => {
   const subdomain = useSubdomain();
   return (
     <AppBar position="static">
+      <Helmet>
+        <title>
+          {subdomain ? `${subdomain.name}` : "Research to the People"}
+        </title>
+      </Helmet>
       <Toolbar>
-        <Helmet>
-          <title>
-            {subdomain
-              ? `${subdomain.name}`
-              : "Research to the People"}
-          </title>
-        </Helmet>
-        <Typography variant="h6" style={{ flexGrow: 1 }}>
-          {subdomain.name}
-        </Typography>
-        <Button color="inherit">Cases</Button>
-        <Button color="inherit">Journal</Button>
-        <Button color="inherit">Give</Button>
+        <Grid container alignItems="center">
+          <Grid item>
+            <Box display="flex" alignItems="center">
+              <img
+                src={subdomain.logo}
+                alt="Logo"
+                style={{ height: 40, marginRight: 16 }}
+              />
+            </Box>
+          </Grid>
+          <Grid item>
+            <Button color="inherit">Cases</Button>
+            <Button color="inherit">Journal</Button>
+          </Grid>
+          <Grid item xs />
+          <Grid item>
+            <Button color="inherit">Log in</Button>
+          </Grid>
+        </Grid>
       </Toolbar>
     </AppBar>
   );
