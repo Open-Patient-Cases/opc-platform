2976f72c8a686e34d7e79e9eb494f108238d75c5
martinappberg <mkjberg@gmail.com>
2024-05-18 12:22:24 -0700

    firebase push

--- frontend/src/firebaseConfig.js
+++ frontend/src/firebaseConfig.js
@@ -1,6 +1,9 @@
 import { initializeApp } from 'firebase/app';
-import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore';
-import { getAuth, connectAuthEmulator } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+
+//import { connectFirestoreEmulator } from 'firebase/firestore';
+//import { connectAuthEmulator } from 'firebase/auth';
 
 const firebaseConfig = {
     apiKey: "AIzaSyBkoB3lwxp5BM9n_Wy3Yo2tMYJ3A-WY8aY",
@@ -23,9 +26,9 @@ const auth = getAuth(app);
 
 console.log(window.location.hostname);
 // Use emulators if in development
-if (window.location.hostname.includes('localhost')) {
-  connectFirestoreEmulator(db, 'localhost', 8080);
-  connectAuthEmulator(auth, 'http://localhost:9099');
-}
+// if (window.location.hostname.includes('localhost')) {
+//   connectFirestoreEmulator(db, 'localhost', 8080);
+//   connectAuthEmulator(auth, 'http://localhost:9099');
+// }
 
 export { db, auth };
