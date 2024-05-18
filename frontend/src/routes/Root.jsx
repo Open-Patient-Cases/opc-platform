23b1aa4f8423111b19edecb562390711c28a3cf1
martinappberg <mkjberg@gmail.com>
2024-05-18 14:25:07 -0700

    fail

--- frontend/src/routes/Root.jsx
+++ frontend/src/routes/Root.jsx
@@ -1,10 +1,15 @@
-import MainContent from "../components/MainContent";
+import { Outlet } from "react-router-dom";
+
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 export default function Root() {
   return (
     <>
       <div>
-        <MainContent />
+        <Header />
+        <Outlet />
+        <Footer />
       </div>
     </>
   );
